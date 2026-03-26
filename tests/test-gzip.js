691429fc3d951c895308972e9d10188beae8c486
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-03-26 21:50:56 +0530

    chore: add tests for gzip size

---
+++ tests/test-gzip.js
@@ -0,0 +1,25 @@
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+
+const files = ['./dist/index.js', './src/index.d.ts'];
+
+
+let totalBytes = 0;
+
+files.forEach(file => {
+  if (fs.existsSync(file)) {
+    const content = fs.readFileSync(file);
+    const gzipped = zlib.gzipSync(content);
+    const bytes = gzipped.length;
+    const kb = (bytes / 1024).toFixed(2);
+    
+    totalBytes += bytes;
+    console.log(`${file}: ${bytes} bytes (${kb} KB)`);
+  } else {
+    console.log(`${file}: Not found!`);
+  }
+});
+
+const totalKb = (totalBytes / 1024).toFixed(2);
+
+console.log(`TOTAL BUNDLE SIZE: ${totalBytes} bytes (${totalKb} KB)`);
